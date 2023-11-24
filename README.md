ff4baa3ad9b3f14cedaee664a613c0eb18b9447c
dev-leticia-costa <leticiaccmarques@hotmail.com>
2023-11-24 14:30:15 -0300

    test routes

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ config package
 npm install --save express
 install node modules (gitignore) - npm install: baixa dependencias novamente 
 create server.js and app.js
+run node server.js (run server)
+test postman get: localhost:3000
+create routes
--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res,next) => {
-    res.status(200).send({
-        mensagem: "Ok! Deu certo, status 200 :)"
-    });
-});
+
+const rotaProdutos = require('./routes/produtos');
+const rotaPedidos = require('./routes/pedidos');
+
+app.use('/produtos', rotaProdutos);
+app.use('/pedidos', rotaPedidos);
+
 module.exports = app;
